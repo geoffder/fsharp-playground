8a069fa71aeff391b7694ac50e0faadbcefe099f
Geoff deRosenroll <geoffderosenroll@gmail.com>
2020-08-03 12:36:50 -0700

    Convert indentation 4 -> 2 spaces.

--- src/Playground/Filters.fs
+++ src/Playground/Filters.fs
@@ -2,8 +2,8 @@ module Filters
 
 let applyFilters fs i =
   let rec apply = function
-    | f :: t -> if f i then apply t else false
-    | [] -> true
+  | f :: t -> if f i then apply t else false
+  | [] -> true
   apply fs
 
 let isDivBy d i = (i % d) = 0
@@ -11,27 +11,27 @@ let isDivs = List.map isDivBy [ 3; 5 ]
 
 let maybeDivBy d i = if (i % d) = 0 then Some i else None
 let maybeDivs =
-    [ for i in [ 3; 5 ] -> maybeDivBy i ]
-    |> List.reduce (fun f1 f2 -> f1 >> Option.bind f2)
+  [ for i in [ 3; 5 ] -> maybeDivBy i ]
+  |> List.reduce (fun f1 f2 -> f1 >> Option.bind f2)
 
 let emptyBind f a =
-    match a with
-    | [e] -> f e
-    | _ -> []
+  match a with
+  | [e] -> f e
+  | _ -> []
 
 let eleDivBy d i = if (i % d) = 0 then [i] else []
 let eleDivs =
-    [ 3; 5 ]
-    |> List.map eleDivBy
-    |> List.reduce (fun f1 f2 -> f1 >> emptyBind f2)
+  [ 3; 5 ]
+  |> List.map eleDivBy
+  |> List.reduce (fun f1 f2 -> f1 >> emptyBind f2)
 
 // fastest, ~800ms for 10M integers
 let testBoolFilter l =
-    List.filter (applyFilters isDivs) l
+  List.filter (applyFilters isDivs) l
 
 // twice as slow as BoolFilter (~1.6s)
 let testMaybeCollect l =
-    l |> List.map maybeDivs |> List.collect Option.toList
+  l |> List.map maybeDivs |> List.collect Option.toList
 
 // only marginally worse than BoolFilter (~900ms)
 let testMaybeChoose l = List.choose maybeDivs l
--- src/Playground/UDPMessages.fs
+++ src/Playground/UDPMessages.fs
@@ -7,9 +7,9 @@ open System.Net.Sockets
 type Agent<'T> = MailboxProcessor<'T>
 
 type Message =
-    | Chat of string
-    | Ping
-    | Pong
+  | Chat of string
+  | Ping
+  | Pong
 
 let (<--) (a: Agent<'T>) msg = a.Post msg
 let (<->) (a: Agent<'T>) msg = a.PostAndReply msg
@@ -17,64 +17,70 @@ let (<-?->) (a: Agent<'T>) (msg, ms) = a.TryPostAndReply (msg, ?timeout = Some m
 let start (agent: Agent<'T> -> Async<unit>) = Agent<'T>.Start agent
 
 let strToBytes (str: string) =
-    str |> System.Text.Encoding.ASCII.GetBytes
+  str |> System.Text.Encoding.ASCII.GetBytes
 
 let bytesToStr (bs: byte array) =
-    bs |> System.Text.Encoding.ASCII.GetString
+  bs |> System.Text.Encoding.ASCII.GetString
 
 let delayAction ms f =
-    async {
-        do! Async.Sleep ms
-        do f ()
-    } |> Async.Start
+  async {
+    do! Async.Sleep ms
+    do f ()
+  } |> Async.Start
 
 let sendingAgent (port: int) = Agent<IPEndPoint * Message>.Start(fun inbox ->
-    let client = new UdpClient (port)
-    let rec loop () = async {
-        let! endpoint, msg = inbox.Receive ()
-        msg
-        |> JsonConvert.SerializeObject
-        |> strToBytes
-        |> fun bs -> client.Send (bs, bs.Length, endpoint)
-        |> ignore
-        return! loop ()
-    }
-    loop ()
+  let client = new UdpClient (port)
+  let rec loop () = async {
+    let! endpoint, msg = inbox.Receive ()
+    msg
+    |> JsonConvert.SerializeObject
+    |> strToBytes
+    |> fun bs -> client.Send (bs, bs.Length, endpoint)
+    |> ignore
+    return! loop ()
+  }
+  loop ()
 )
 
 let receiving (port: int) (sender: Agent<IPEndPoint * Message>) =
-    let client = new UdpClient (port)
-    let rec loop i = async {
-        let! result = client.ReceiveAsync() |> Async.AwaitTask
-        do result.Buffer
-        |> bytesToStr
-        |> JsonConvert.DeserializeObject<Message>
-        |> function
-           | Chat str -> printfn "%A says: %s" result.RemoteEndPoint str
-           | Ping ->
-               printfn "Ping from %A!" result.RemoteEndPoint
-               sender.Post (result.RemoteEndPoint, Pong)
-           | Pong ->
-               printfn "Pong from %A!" result.RemoteEndPoint
-        do printfn "count = %i" i
-        return! loop (i + 1)
-    }
-    loop 0 |> Async.Start
+  let client = new UdpClient (port)
+  let rec loop i = async {
+    let! result = client.ReceiveAsync () |> Async.AwaitTask
+    do result.Buffer
+    |> bytesToStr
+    |> JsonConvert.DeserializeObject<Message>
+    |> function
+       | Chat str -> printfn "%A says: %s" result.RemoteEndPoint str
+       | Ping ->
+         printfn "Ping from %A!" result.RemoteEndPoint
+         sender <-- (result.RemoteEndPoint, Pong)
+       | Pong ->
+         printfn "Pong from %A!" result.RemoteEndPoint
+    do printfn "count = %i" i
+    return! loop (i + 1)
+  }
+  loop 0 |> Async.Start
 
 let ping (sender: Agent<IPEndPoint * Message>) port =
-    sender <-- (IPEndPoint (IPAddress.Any, port), Ping)
+  sender <-- (IPEndPoint (IPAddress.Any, port), Ping)
+
+let pingLocal (sender: Agent<IPEndPoint * Message>) port =
+  sender <-- (IPEndPoint (IPAddress.Parse "127.0.0.1", port), Ping)
+
+let ping192 (sender: Agent<IPEndPoint * Message>) port =
+  sender <-- (IPEndPoint (IPAddress.Parse "192.168.0.14", port), Ping)
 
 let chat (sender: Agent<IPEndPoint * Message>) port str =
-    sender <-- (IPEndPoint (IPAddress.Any, port), Chat str)
+  sender <-- (IPEndPoint (IPAddress.Any, port), Chat str)
 
 let slowHello (sender: Agent<IPEndPoint * Message>) port =
-    fun () -> chat sender port "Hello"
-    |> delayAction 5000
+  fun () -> chat sender port "Hello"
+  |> delayAction 5000
 
-let blast port sender = async {
-    for i in 1..10 do
-        chat sender port (sprintf "%i" i)
-        // do! Async.Sleep 1
+let blast n port sender = async {
+  for i in 1..n do
+    chat sender port (sprintf "%i" i)
+    // do! Async.Sleep 1
 }
 
 // List.map (blast 4000) [s1; s2] |> Async.Parallel |> Async.RunSynchronously;;
